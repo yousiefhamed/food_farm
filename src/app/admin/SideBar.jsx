31e34f7c970a48b39583f166e2290d971f8bca2e
Youssef Hamed <yousiefhamed1@gmail.com>
2024-06-28 08:23:22 +0300

    handling mobile responsiveness of add product functionality

--- src/app/admin/SideBar.jsx
+++ src/app/admin/SideBar.jsx
@@ -1,15 +1,18 @@
 "use client";
 import Link from "next/link";
 import AddProduct from "./AddProducts";
+import { useState } from "react";
 
-export default function SideBar({ menu, chgComp }) {
+export default function SideBar({ menu, chgComp, showMenu }) {
   return (
     <div
       style={{
         height: "calc(100vh - 60px)",
         background: "linear-gradient(135deg, lightgreen, lightblue)",
       }}
-      className="flex flex-col justify-start items-center gap-5 overflow-y-auto w-[400px] h-screen fixed top-0 left-0 mt-[60px] pt-5 rounded-tr-3xl rounded-br-3xl"
+      className={`flex flex-col justify-start items-center gap-5 overflow-y-auto lg:w-[400px] w-[250px] h-screen fixed top-0 left-0 mt-[60px] pt-16 lg:pt-5 rounded-tr-3xl rounded-br-3xl ${
+        showMenu ? "translate-x-0" : "-translate-x-full"
+      } md:translate-x-0 transition-all z-40`}
     >
       {menu.map(({ title }) => (
         <div
--- src/app/admin/signIn/page.js
+++ src/app/admin/signIn/page.js
@@ -1,9 +1,11 @@
 "use client";
 import { useState } from "react";
+import { CiMenuFries } from "react-icons/ci";
 import SideBar from "../SideBar";
 import AddProduct from "../AddProducts";
 
 export default function page() {
+  const [showMenu, setShowMenu] = useState(false);
   const [renderComp, setRenderComp] = useState("Add Product");
   const menu = [
     { title: "Add Product", component: <AddProduct /> },
@@ -13,8 +15,14 @@ export default function page() {
   ];
 
   return (
-    <main className="pt-20 min-h-screen pl-[400px] px-5 w-full bg-green-50">
-      <SideBar menu={menu} chgComp={setRenderComp} />
+    <main className="pt-20 min-h-screen lg:pl-[400px] md:pl-[250px] pl-0 px-5 w-full bg-green-50">
+      <button
+        className="md:hidden inline-block fixed top-[120px] left-0 z-50 p-5 rounded-tr-full rounded-br-full bg-white"
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        <CiMenuFries />
+      </button>
+      <SideBar menu={menu} chgComp={setRenderComp} showMenu={showMenu} />
       <section role="form" className="flex justify-center items-center mb-10">
         {menu.find(({ title }) => title === renderComp).component}
       </section>
